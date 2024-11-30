337a1397096be03a07576b80e02b7d735f7ee71f
Sunhadi <ahmadsunhadikamil65@gmail.com>
2024-12-01 03:11:15 +0700

    Pesan vercel update

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-      outDir: 'dist', // Pastikan output berada di folder dist
-    },
     plugins: [
-      laravel({
-        input: ['resources/css/app.css', 'resources/js/app.js'],
-        refresh: true,
-      }),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
     ],
-  });
-  
+    build: {
+        outDir: 'dist',  // Menyimpan hasil build di luar public/build
+    },
+});
