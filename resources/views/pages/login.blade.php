b4aad5851094739c2d8d5904e1d5efbaad450738
Sunhadi <ahmadsunhadikamil65@gmail.com>
2024-12-04 17:21:00 +0700

    perbaikan

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -32,7 +32,7 @@
     <meta name="description" content=""/>
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{asset('sneat/img/favicon/favicon.ico')}}"/>
+    <link rel="icon" type="image/x-icon" href="{{asset('sneat/logo_kebumen.png')}}"/>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
